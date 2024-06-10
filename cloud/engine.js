b0f2f583ee2713a8935c2596fb7ab28007e8a33f
chris <chrsvdmrw@gmail.com>
2024-06-10 15:43:45 +0200

    include script file

--- cloud/engine.js
+++ cloud/engine.js
@@ -7,21 +7,31 @@ const routes = require('../web/routes')
 const hostname = 'localhost';
 const port = 1820;
 
-
-
-
 const server = createServer(async (req, res) => {
     res.statusCode = 200;
     const pathname = url.parse(req.url).pathname;
+    const pagePath = '/Users/Chris/dev/simply/web/pages/about/about.html'
+    const pageFolderPath = '/Users/Chris/dev/simply/web/pages/about'
 
-    
-const pageContent = await fs.readFile('/Users/Chris/dev/simply/web/pages/about/about.html', 'utf8')
+    // await getComponents()
 
-  res.setHeader('Content-Type', 'text/html');
-  res.end(`<div id='simply-app'>${pageContent}</div>`);
+    const scripts = await fs.readFile(`${pageFolderPath}/script.js`)
+
+    const pageContent = await fs.readFile(pagePath, 'utf8')
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`<script>${scripts}</script>`)
+    res.end(`<div id='simply-app'>${pageContent}</div>`);
 });
 
 server.listen(port, hostname, () => {
-    console.log(routes)
-  console.log(`Server running at http://${hostname}:${port}`);
+    console.log(`🌈 simply server running at http://${hostname}:${port}`);
+    console.log(`------------------------------------------------------`)
 });
+
+
+const getComponents = async () => {
+    const componentPath = '/Users/Chris/dev/simply/web/components'
+    const dirContents = await fs.readdir(componentPath)
+    console.log(dirContents[0])
+}
--- web/pages/about/script.js
+++ web/pages/about/script.js
@@ -1,26 +1,30 @@
-const contactForm = {
-    fields: [{
-        type: 'string',
-        name: 'contact-name',
-        label: 'Name'
-    }, {
-        type: 'email',
-        name: 'contact-email',
-        label: 'Email Address'
-    }, {
-        type: 'textarea',
-        name: 'contact-message',
-        label: 'Message'
-    }],
-    buttons: [
-        {
-            type: 'submit',
-            label: 'Send Message',
-            onClick: onContactFormSubmitButtonClick()
-        }
-    ]
-}
+// const contactForm = {
+//     fields: [{
+//         type: 'string',
+//         name: 'contact-name',
+//         label: 'Name'
+//     }, {
+//         type: 'email',
+//         name: 'contact-email',
+//         label: 'Email Address'
+//     }, {
+//         type: 'textarea',
+//         name: 'contact-message',
+//         label: 'Message'
+//     }],
+//     buttons: [
+//         {
+//             type: 'submit',
+//             label: 'Send Message',
+//             onClick: onContactFormSubmitButtonClick()
+//         }
+//     ]
+// }
 
-const onContactFormSubmitButtonClick = async (ContactFormInput) => {
-    services.sendEmail(ContactFormInput)
+// const onContactFormSubmitButtonClick = async (ContactFormInput) => {
+//     services.sendEmail(ContactFormInput)
+// }
+
+const onFooButtonClick = () => {
+    console.log("This comes the adjacent script file")
 }
