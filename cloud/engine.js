bb9703199a0245f781cd7c3e542b05b6a2f2eef0
chris <chrsvdmrw@gmail.com>
2024-06-26 07:31:21 +0200

    combine js and html within sandbox

--- cloud/engine.js
+++ cloud/engine.js
@@ -1,37 +1,65 @@
-const { createServer } = require('node:http');
-const url = require('node:url')
-const fs = require('node:fs/promises');
+// Create a new isolate limited to 128MB
+const ivm = require('isolated-vm');
+const isolate = new ivm.Isolate({ memoryLimit: 128 });
 
-const routes = require('../web/routes')
- 
+const fs = require('node:fs/promises');
+const url = require('node:url')
+const { createServer } = require('node:http');
+
 const hostname = 'localhost';
 const port = 1820;
 
 const server = createServer(async (req, res) => {
     res.statusCode = 200;
     const pathname = url.parse(req.url).pathname;
-    const pagePath = '/Users/Chris/dev/simply/web/pages/about/about.html'
-    const pageFolderPath = '/Users/Chris/dev/simply/web/pages/about'
-
-    // await getComponents()
-
-    const scripts = await fs.readFile(`${pageFolderPath}/script.js`)
-
-    const pageContent = await fs.readFile(pagePath, 'utf8')
+    console.log(pathname) 
 
     res.setHeader('Content-Type', 'text/html');
-    res.write(`<script>${scripts}</script>`)
-    res.end(`<div id='simply-app'>${pageContent}</div>`);
+  
+    const compilerContext = isolate.createContextSync();
+
+    const jail = compilerContext.global;
+
+    jail.setSync('global', jail.derefInto());
+
+    jail.setSync('combine', () => {
+        const pagePath = '/Users/Chris/dev/simply/web/pages/about/about.html'
+        const pageFolderPath = '/Users/Chris/dev/simply/web/pages/about'
+
+        const getContent = new Promise(async (resolve, rej) => {
+            const scripts= await fs.readFile(`${pageFolderPath}/script.js`)
+            const pageContent =  await fs.readFile(pagePath, 'utf8')
+            resolve([scripts, pageContent])
+        })
+
+        getContent.then(([scripts, output]) => {
+            const func = scriptString(scripts, output)
+            const pageContent = eval(func)()
+
+            res.write(`<script>${scripts}</script>`)
+            res.end(`<div id='simply-app'>${pageContent}</div>`);
+        })
+        
+    });
+
+    compilerContext.eval('combine()');    
 });
 
 server.listen(port, hostname, () => {
-    console.log(`🌈 simply server running at http://${hostname}:${port}`);
+    console.log(`🌈 simply p.o.c. running at http://${hostname}:${port}`);
     console.log(`------------------------------------------------------`)
 });
 
 
-const getComponents = async () => {
-    const componentPath = '/Users/Chris/dev/simply/web/components'
-    const dirContents = await fs.readdir(componentPath)
-    console.log(dirContents[0])
-}
+const scriptString = (scripts, output) => {
+
+    // Removes html comments
+    output = output.replace(/<\!--.*?-->/g, "");
+
+            return `() => {
+                ${scripts}
+            return (\`
+            ${ output }
+            \`
+        )}`
+        }
