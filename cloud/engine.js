239a96242b49a4335e90b32ba2427271b5e12544
chrisvdm <chrsvdmrw@gmail.com>
2024-08-21 19:56:28 +0200

    define routes file

--- cloud/engine.js
+++ cloud/engine.js
@@ -1,45 +1,36 @@
-// Create a new isolate limited to 128MB
-const ivm = require('isolated-vm');
-const isolate = new ivm.Isolate({ memoryLimit: 128 });
+// Create a new isolate limited to 128MB 
+// Causes segmentation error
+// const ivm = require('isolated-vm');
+// const isolate = new ivm.Isolate({ memoryLimit: 128 }); 
 
 const fs = require('node:fs/promises');
 const url = require('node:url')
 const path = require("node:path");
 const { createServer } = require('node:http');
+const { lutimesSync } = require('node:fs');
 
 const hostname = 'localhost';
 const port = 1820;
 
- let route = ''
+// route and dirPAth get defined  further down
+let route = ''
+let dirPath = ''
 
 const server = createServer(async (req, res) => {
     res.statusCode = 200;
-   
-    const pathnamefirst = url.parse(req.url).pathname;
-    const pathname = path.join(__dirname, `../web/pages${route}${req.url}`)
-    console.log("pathname", pathname) 
-    console.log('req.url', req.url)
-    console.log(url.hostname)
 
-    const pages = await getPageNames()
-    
+    const routes = await getRoutes()
+    const routesList = Object.getOwnPropertyNames(routes)
 
     res.setHeader('Content-Type', 'text/html');
   
-    const compilerContext = isolate.createContextSync();
-
-    const jail = compilerContext.global;
-
-    jail.setSync('global', jail.derefInto());
-
-    jail.setSync('combine', () => {
         if (req.url === '/favicon.ico') {
             return
         }
 
         if (req.url === '/script.js') {
             new Promise(async (resolve) => {
-                const scriptPath = path.join(__dirname, `../web/pages${route}/script.js`)
+                const scriptPath = `${dirPath}/script.js`
                 const scripts = await fs.readFile(scriptPath)  
                 resolve(scripts)
             }).then(result => {
@@ -47,12 +38,14 @@ const server = createServer(async (req, res) => {
                 res.end(result);
             })
             
-        } else if (pages.includes(req.url)) {
-            route = req.url
-            const pagePath = `${pathname}${req.url}.html`
+        } else if (routesList.includes(req.url)) {
+            route = routes[req.url].name
+            dirPath = path.join(__dirname, `../web/pages/${route}`)
+
+            const pagePath = `${dirPath}/${route}.html`
             
             const getContent = new Promise(async (resolve, rej) => {
-                const scripts = await fs.readFile(`${pathname}/script.js`)  
+                const scripts = await fs.readFile(`${dirPath}/script.js`)  
                 const pageContent =  await fs.readFile(pagePath, 'utf8')
                 resolve([scripts, pageContent])
             })
@@ -65,12 +58,7 @@ const server = createServer(async (req, res) => {
                 res.write(`<div id='simply-app'>${pageContent}</div>`);
                 res.end(`\n<script src="/script.js"></script>`)
             })
-        }
-
-        
-    });
-
-    compilerContext.eval('combine()');    
+        } 
 });
 
 server.listen(port, hostname, () => {
@@ -93,9 +81,8 @@ const scriptString = (scripts, output) => {
     }`
 }
 
-const getPageNames = async() => {
-    const dirContent = await fs.readdir(path.join(__dirname, '../web/pages'), { withFileTypes: true })
-    return dirContent.map(i => {
-        return `/${i.name}`
-    })
+const getRoutes = async() => {
+    const routesMod = await import('../web/routes.mjs')
+    const routes = routesMod.default
+    return routes
 }
---
+++ web/routes.mjs
@@ -0,0 +1,26 @@
+const routes =  {
+        '/about': {
+            to: '/about',
+            name: 'about',
+                title: 'About'
+            },
+        '/': {
+            to: '/',
+            name: 'home',
+            title: 'Home',
+            // preload: async () => {
+            //     const posts = await service.fetch.posts({ limit: 5 })
+            //     context['posts'] = posts
+            // }
+        },
+        // '/post': {
+        //     to: '/:id',
+        //     // title: context.posts(params.id).title,
+        //     // preload: async () => {
+        //     //     const post = await service.retrieve.post(id)
+        //     //     context['post'] = post
+        //     // }
+        // }}
+};
+
+export default routes
